4fabbb073ab5889640d6274f555952624069718d
Андрей Макарычев <makarychev@reg.ru>
2018-04-18 09:51:44 +0400

    update legacy rules

--- packages/eslint-config-regru/legacy.js
+++ packages/eslint-config-regru/legacy.js
@@ -10,7 +10,7 @@ module.exports = {
         ecmaVersion : 2017,
         sourceType  : 'module',
     },
-    extends : [ 'eslint:recommended' ],
+    extends : [ 'eslint:recommended', 'plugin:vue/recommended' ],
     plugins : [
         'regru',
         'vue',
